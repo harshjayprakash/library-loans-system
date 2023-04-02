974bc84eea114486880fa80ec13a653f0d34fa44
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 08:19:27 +0100

    Added javafx graphics and base.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module winchester.library {
+    requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
             
                             
     opens winchester.library to javafx.fxml;
