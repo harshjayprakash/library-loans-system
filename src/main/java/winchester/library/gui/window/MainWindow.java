292026999affa0baceaac6a96c640eef0349f393
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 17:55:24 +0100

    Added main components.

--- src/main/java/winchester/library/gui/window/MainWindow.java
+++ src/main/java/winchester/library/gui/window/MainWindow.java
@@ -1,6 +1,17 @@
 package winchester.library.gui.window;
 
+import winchester.library.gui.component.HeaderPane;
+import winchester.library.gui.component.SidePane;
+import winchester.library.gui.component.StatusPane;
+import winchester.library.gui.view.Views;
+import winchester.library.gui.view.ViewsManager;
+
 public class MainWindow extends WindowBase {
+
+    private HeaderPane headerPane;
+    private SidePane sidePane;
+    private StatusPane statusPane;
+    private ViewsManager viewsManager;
 
     public MainWindow() {
         super();
@@ -16,11 +27,17 @@ public class MainWindow extends WindowBase {
 
     @Override
     protected void initialiseControls() {
-
+        this.headerPane = new HeaderPane(Views.NONE_WITH_SIDEBAR, Views.NONE);
+        this.sidePane = new SidePane(null);
+        this.statusPane = new StatusPane();
+        this.viewsManager = new ViewsManager();
     }
 
     @Override
     protected void addComponentsToStage() {
-
+        this.baseLayout.setLeft(this.sidePane);
+        this.baseLayout.setBottom(this.statusPane);
+        this.baseLayout.setTop(this.headerPane);
+        this.baseLayout.setCenter(this.viewsManager);
     }
 }
