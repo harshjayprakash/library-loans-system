c102e7e2f7c36408d37c76b456428ef98adcf715
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 13:13:24 +0100

    Added Stylesheet with Manager.

--- src/main/java/winchester/library/Main.java
+++ src/main/java/winchester/library/Main.java
@@ -2,12 +2,13 @@ package winchester.library;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import winchester.library.gui.style.StylesheetManager;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-
+        StylesheetManager.getInstance().addStylesheet("/winchester/library/gui/style/Base.css");
     }
 
     public static void main(String[] args) {
