bb15f039f4491d6b8edb08a5619c7932f1649adf
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 14:29:40 +0100

    Added `@Override` methods.

---
+++ src/main/java/winchester/library/gui/window/LoginWindow.java
@@ -0,0 +1,24 @@
+package winchester.library.gui.window;
+
+import javafx.scene.layout.VBox;
+
+public class LoginWindow extends WindowBase {
+    public LoginWindow() {
+        super(new VBox(), 450, 700);
+    }
+
+    @Override
+    protected void initialiseLayouts() {
+
+    }
+
+    @Override
+    protected void initialiseControls() {
+
+    }
+
+    @Override
+    protected void addComponentsToStage() {
+
+    }
+}
