c1ab61cfa2bde60ba367613674fc19ceb69351c7
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 19:54:38 +0100

    Added basic attributes.

--- src/main/java/winchester/library/data/DatabaseConnectionManager.java
+++ src/main/java/winchester/library/data/DatabaseConnectionManager.java
@@ -1,4 +1,40 @@
 package winchester.library.data;
 
 public class DatabaseConnectionManager {
+
+    private final static DatabaseConnectionManager instance = new DatabaseConnectionManager();
+    private String url;
+    private String username;
+    private String password;
+
+    private DatabaseConnectionManager() { }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static DatabaseConnectionManager getInstance() {
+        return DatabaseConnectionManager.instance;
+    }
+
 }
