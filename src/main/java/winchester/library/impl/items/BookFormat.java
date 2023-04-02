0bb9cea13224bb75a39c0544bf241da48989f8ba
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 09:40:54 +0100

    Created `BookFormat` enumeration.

---
+++ src/main/java/winchester/library/impl/items/BookFormat.java
@@ -0,0 +1,25 @@
+package winchester.library.impl.items;
+
+public enum BookFormat {
+    AUDIO(11, "Audio Book"),
+    PHYSICAL(12, "Physical Book"),
+    LARGE_PRINT(13, "Large Print Book"),
+    ELECTRONIC(14, "Electronic Book");
+
+    private final int identifier;
+    private final String text;
+
+    BookFormat(int identifier, String text) {
+        this.identifier = identifier;
+        this.text = text;
+    }
+
+    public int getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
+}
