40cb6f488acdd137398ca0d26fdca66f58445ccd
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 17:10:45 +0100

    Updated stylesheet name.

--- src/main/java/winchester/library/Main.java
+++ src/main/java/winchester/library/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        StylesheetManager.getInstance().addStylesheet("base.css");
+        StylesheetManager.getInstance().addStylesheet("style.css");
         IndividualViewWindow loginWindow = new IndividualViewWindow(Views.LOGIN, null);
         loginWindow.show();
     }
