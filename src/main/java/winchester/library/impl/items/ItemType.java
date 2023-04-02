86a748fbc9bb2c7512b4f2b1e0279d94b0cbafff
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 11:33:12 +0100

    Created `ItemType` enumeration.

---
+++ src/main/java/winchester/library/impl/items/ItemType.java
@@ -0,0 +1,23 @@
+package winchester.library.impl.items;
+
+public enum ItemType {
+    BOOK(1, "Book"),
+    FILM(2, "Film");
+
+    private final int identifier;
+    private final String value;
+
+    ItemType(int identifier, String value) {
+        this.identifier = identifier;
+        this.value = value;
+    }
+
+    public int getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
