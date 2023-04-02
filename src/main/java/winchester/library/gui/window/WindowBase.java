cbc72f0189d21e19c535b7db6edec7b2f88d08ca
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 13:15:06 +0100

    Added imports.

--- src/main/java/winchester/library/gui/window/WindowBase.java
+++ src/main/java/winchester/library/gui/window/WindowBase.java
@@ -1,6 +1,9 @@
 package winchester.library.gui.window;
 
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import winchester.library.gui.style.StylesheetManager;
 
 public abstract class WindowBase extends Stage {
     public WindowBase() {
