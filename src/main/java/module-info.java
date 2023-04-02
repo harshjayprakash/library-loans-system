816452aae1624817f2a29698c6e7846cc81381d6
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 13:12:40 +0100

    Updated Dependencies.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module winchester.library {
     requires javafx.controls;
     requires javafx.fxml;
     requires transitive javafx.graphics;
+    requires mysql.connector.j;
+    requires org.jetbrains.annotations;
             
                             
     opens winchester.library to javafx.fxml;
