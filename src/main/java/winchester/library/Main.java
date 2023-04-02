7aa2e990d8c44746d78faefca48779d18bc7898f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 16:11:58 +0100

    Added Login View to start.

--- src/main/java/winchester/library/Main.java
+++ src/main/java/winchester/library/Main.java
@@ -3,12 +3,16 @@ package winchester.library;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import winchester.library.gui.style.StylesheetManager;
+import winchester.library.gui.view.Views;
+import winchester.library.gui.window.IndividualViewWindow;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        StylesheetManager.getInstance().addStylesheet("/winchester/library/gui/style/Base.css");
+        StylesheetManager.getInstance().addStylesheet("base.css");
+        IndividualViewWindow loginWindow = new IndividualViewWindow(Views.LOGIN, null);
+        loginWindow.show();
     }
 
     public static void main(String[] args) {
