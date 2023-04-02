faf18c04ff47ed2ce5a280418d00dc942a8ba574
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 09:28:05 +0100

    Created `Customer` class.

---
+++ src/main/java/winchester/library/impl/users/Customer.java
@@ -0,0 +1,44 @@
+package winchester.library.impl.users;
+
+import java.util.ArrayList;
+
+public class Customer extends User {
+    private int overdueFeesPence;
+    private final ArrayList<?> loans;
+
+    public Customer(String firstName, String lastName, String postalCode) {
+        super(firstName, lastName, postalCode);
+        this.overdueFeesPence = 0;
+        this.loans = new ArrayList<>();
+    }
+
+    public int getOverdueFeesAsPence() {
+        return this.overdueFeesPence;
+    }
+
+    public double getOverdueFeesAsPounds() {
+        return this.overdueFeesPence * 0.01;
+    }
+
+    public void setOverdueFees(int feesPence) {
+        this.overdueFeesPence = feesPence;
+    }
+
+    public ArrayList<?> getLoans() {
+        return this.loans;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                %s
+                Overdue Fees: £%.2f
+                Loans: null
+               """.formatted(super.toString(), this.getOverdueFeesAsPounds());
+    }
+
+    @Override
+    public UserType getType() {
+        return UserType.CUSTOMER;
+    }
+}
