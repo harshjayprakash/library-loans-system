048c539fca78bc12c1e5bb9265bbf7d69ef5f39e
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 17:43:19 +0100

    Removed redundant required statements.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,8 @@
 module winchester.library {
-    requires javafx.base;
     requires javafx.controls;
-    requires javafx.fxml;
     requires transitive javafx.graphics;
-    requires mysql.connector.j;
-    requires org.jetbrains.annotations;
-            
-                            
+
+
     opens winchester.library to javafx.fxml;
     exports winchester.library;
 }
