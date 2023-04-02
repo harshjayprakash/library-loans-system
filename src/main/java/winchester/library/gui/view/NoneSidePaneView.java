726597a42befe93052635beecaae14caea2ba552
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 18:58:19 +0100

    Added controls.

--- src/main/java/winchester/library/gui/view/NoneSidePaneView.java
+++ src/main/java/winchester/library/gui/view/NoneSidePaneView.java
@@ -1,6 +1,13 @@
 package winchester.library.gui.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+
 public class NoneSidePaneView extends View {
+
+    private Label messageLabel;
+    private Label informationLabel;
 
     public NoneSidePaneView() {
         super();
@@ -11,16 +18,21 @@ public class NoneSidePaneView extends View {
 
     @Override
     protected void initialiseLayouts() {
-
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(15));
     }
 
     @Override
     protected void initialiseControls() {
-
+        this.messageLabel = new Label();
+        this.messageLabel.setId("text-bold");
+        this.messageLabel.setText("No Content Shown.");
+        this.informationLabel = new Label();
+        this.informationLabel.setText("Please click on one of the views on the side.");
     }
 
     @Override
     protected void addComponentsToView() {
-
+        this.getChildren().addAll(this.messageLabel, this.informationLabel);
     }
 }
