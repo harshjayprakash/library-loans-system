dd7c5077cce325057a4d6c5cdca0efe1cbaa1f4f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-02 17:51:09 +0100

    Added event handler.

--- src/main/java/winchester/library/gui/component/StatusPane.java
+++ src/main/java/winchester/library/gui/component/StatusPane.java
@@ -3,9 +3,12 @@ package winchester.library.gui.component;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.TextAlignment;
 import winchester.library.gui.style.StylesheetSetter;
+import winchester.library.gui.view.Views;
+import winchester.library.gui.window.IndividualViewWindow;
 
 public class StatusPane extends BorderPane {
     private Label versionNumberLabel;
@@ -37,7 +40,10 @@ public class StatusPane extends BorderPane {
     }
 
     private void bindEventHandlers() {
-
+        this.databaseStatusLabel.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            IndividualViewWindow databaseInformationView = new IndividualViewWindow(Views.DATABASE_INFORMATION, null);
+            databaseInformationView.show();
+        });
     }
 
     private void addComponentsToPane() {
