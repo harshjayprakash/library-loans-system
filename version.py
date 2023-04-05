61b6b00c40519bd52991a4ab425581887aeaaa5d
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-05 12:29:59 +0100

    Organised version updater

--- version.py
+++ version.py
@@ -1,7 +1,21 @@
-import toml
+"""
+[THIS FILE IS NOT FOR MARKING]
+This python file is for automatically incrementing the version number
+in the project.toml file every time that maven compiles the java
+application.
+"""
+error_occurred: bool = False
 
-data = toml.load("project.toml")
-data["project"]["version"]["build"] += 1
-file = open("project.toml", 'w')
-toml.dump(data, file)
-file.close()
+try:
+    import toml
+except ImportError:
+    print("Failed to import toml library")
+    error_occurred = True
+
+PROJECT_TOML_FILE = "project.toml"
+
+if not error_occurred:
+    data = toml.load(PROJECT_TOML_FILE)
+    data["project"]["version"]["build"] += 1
+    with open(PROJECT_TOML_FILE, 'w') as file:
+        toml.dump(data, file)
