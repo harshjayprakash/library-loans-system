1f1ec4382e16174fc06b70764bb4732d94311735
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-05 12:21:34 +0100

    Added `LoansManager` functionality.

--- src/main/java/winchester/library/impl/loans/LoansManager.java
+++ src/main/java/winchester/library/impl/loans/LoansManager.java
@@ -1,4 +1,30 @@
 package winchester.library.impl.loans;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class LoansManager {
+
+    private final ArrayList<Loan> loans;
+
+    public LoansManager() {
+        this.loans = new ArrayList<>();
+    }
+
+    public void addLoan(Loan loan) {
+        if (Objects.isNull(loan)) { return; }
+        this.loans.add(loan);
+    }
+
+    public void addLoans(Loan... loans) {
+        for (Loan loan : loans) {
+            if (Objects.isNull(loan)) { continue; }
+            this.loans.add(loan);
+        }
+    }
+
+    public ArrayList<Loan> getLoans() {
+        return this.loans;
+    }
+
 }
