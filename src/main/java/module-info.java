b84f59b678dea9172c385303d0bbdb5ea28fa291
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-05 12:20:53 +0100

    Added dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module winchester.library {
     requires javafx.controls;
     requires transitive javafx.graphics;
+    requires org.jetbrains.annotations;
 
 
     opens winchester.library to javafx.fxml;
