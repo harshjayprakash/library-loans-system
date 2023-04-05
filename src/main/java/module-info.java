ecc393c8212113eb714979714b327b2fdca35cf3
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-05 12:41:45 +0100

    Added java dependencies.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module winchester.library {
     requires javafx.controls;
     requires transitive javafx.graphics;
     requires org.jetbrains.annotations;
+    requires java.base;
+    requires java.sql;
 
 
     opens winchester.library to javafx.fxml;
