53e0769c360cc97080d3adffbc5b471838a4bc4e
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-07 19:29:51 +0100

    Created `Metadata` singleton class.

---
+++ src/main/java/winchester/library/meta/Metadata.java
@@ -0,0 +1,25 @@
+package winchester.library.meta;
+
+public class Metadata {
+
+    private static final Metadata instance = new Metadata();
+    private final String programName = "Winchester Library Services Technical Preview";
+    private final String programVersion = "1.2.x.x";
+
+    private Metadata() {
+
+    }
+
+    public static Metadata getInstance() {
+        return Metadata.instance;
+    }
+
+    public String getProgramName() {
+        return this.programName;
+    }
+
+    public String getProgramVersionNumber() {
+        return this.programVersion;
+    }
+
+}
