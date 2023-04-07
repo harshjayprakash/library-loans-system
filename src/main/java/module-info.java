24f6598f31d4ebb4aaa7007857c6c9264af51df1
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-07 19:02:51 +0100

    Organised imports.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module winchester.library {
+    requires java.base;
+    requires java.sql;
     requires javafx.controls;
     requires transitive javafx.graphics;
     requires org.jetbrains.annotations;
-    requires java.base;
-    requires java.sql;
 
-
-    opens winchester.library to javafx.fxml;
+    opens winchester.library to javafx.controls;
     exports winchester.library;
 }
