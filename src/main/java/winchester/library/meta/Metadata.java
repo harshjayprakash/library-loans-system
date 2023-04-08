a75373a035f9fbe2a08e6af24751e4b83b2f8b01
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-08 11:37:48 +0100

    Changed version to placeholder text.

--- src/main/java/winchester/library/meta/Metadata.java
+++ src/main/java/winchester/library/meta/Metadata.java
@@ -4,7 +4,7 @@ public final class Metadata {
 
     private static final Metadata instance = new Metadata();
     private final String programName = "Winchester Library Services Technical Preview";
-    private final String programVersion = "1.2.x.x";
+    private final String programVersion = "::meta::Metadata->programVersion";
 
     private Metadata() {
 
