b9aacbafc89a3d1bb1c086683188bb869d6c4449
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-11 12:53:41 +0100

    Removed all attributes.

--- src/main/java/winchester/library/data/model/items/Item.java
+++ src/main/java/winchester/library/data/model/items/Item.java
@@ -1,27 +1,8 @@
 package winchester.library.data.model.items;
 
 public abstract class Item {
-    protected final int identifier;
-    protected final String name;
-    protected final int releaseYear;
 
-    public Item(int identifier, String name, int releaseYear) {
-        this.identifier = identifier;
-        this.name = name;
-        this.releaseYear = releaseYear;
-    }
-
-    public int getIdentifier() {
-        return this.identifier;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getReleaseYear() {
-        return this.releaseYear;
-    }
+    public Item() { }
 
     public abstract ItemType getType();
 }
