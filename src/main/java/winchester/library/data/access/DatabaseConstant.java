5855f26d4618485f0c658456e0c280a24d719d47
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-16 16:11:10 +0100

    Renamed.

--- src/main/java/winchester/library/data/access/DatabaseConstant.java
+++ src/main/java/winchester/library/data/access/DatabaseConstant.java
@@ -1,15 +1,15 @@
 package winchester.library.data.access;
 
-public enum DatabaseConstants {
+public enum DatabaseConstant {
     CONNECTION_SUCCESSFUL(0, "Database connection successful"),
     DRIVER_NOT_FOUND(1, "Failed to find MySQL JDBC Connector Driver"),
-    NOT_ACCESSIBLE(2, "Error accessing the database"),
-    UNKNOWN_ERROR(3, "Unknown Error");
+    NOT_ACCESSIBLE(7, "Error accessing the database"),
+    UNKNOWN_ERROR(10, "Error");
 
     private final int identifier;
     private final String value;
 
-    DatabaseConstants(int identifier, String value) {
+    DatabaseConstant(int identifier, String value) {
         this.identifier = identifier;
         this.value = value;
     }
@@ -23,8 +23,8 @@ public enum DatabaseConstants {
         return this.value;
     }
 
-    public static DatabaseConstants getFromIdentifier(int identifier) {
-        for (DatabaseConstants constant : DatabaseConstants.values()) {
+    public static DatabaseConstant getFromIdentifier(int identifier) {
+        for (DatabaseConstant constant : DatabaseConstant.values()) {
             if (constant.identifier == identifier) {
                 return constant;
             }
