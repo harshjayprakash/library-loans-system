0c59ee1f30a64e7aa306e2dcce2e1c017e8e2197
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-16 16:11:53 +0100

    Created `ActionPane` class.

---
+++ src/main/java/winchester/library/presentation/component/ActionPane.java
@@ -0,0 +1,26 @@
+package winchester.library.presentation.component;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class ActionPane extends HBox {
+
+    private TextField searchField;
+    private Button searchButton;
+
+    public ActionPane() {
+        super();
+        this.initialiseControls();
+        this.addComponentsToPane();
+    }
+
+    private void initialiseControls() {
+
+    }
+
+    private void addComponentsToPane() {
+
+    }
+
+}
