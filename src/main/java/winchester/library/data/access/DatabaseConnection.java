8b72e9af5acc877607711f251c2e7f73a0cbe6f0
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-18 11:07:06 +0100

    Created `DatabaseConnection` class.

---
+++ src/main/java/winchester/library/data/access/DatabaseConnection.java
@@ -0,0 +1,64 @@
+package winchester.library.data.access;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.util.Optional;
+
+public class DatabaseConnection {
+
+    private Connection connection;
+
+    public DatabaseConnection() { }
+
+    private void loadDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException exception) {
+            System.err.printf("%s : %s%n", DatabaseConstant.DRIVER_NOT_FOUND, exception.getMessage());
+        }
+    }
+
+    public void establish(DatabaseCredentials credentials) {
+        try {
+            this.loadDriver();
+            this.connection = DriverManager.getConnection(
+                    credentials.getUrl(), credentials.getUsername(), credentials.getPassword());
+        }
+        catch (SQLTimeoutException exception) {
+            System.err.printf("%s : %s%n", DatabaseConstant.CONNECTION_TIMEOUT, exception.getMessage());
+        }
+        catch (SQLException exception) {
+            System.err.printf("%s : %s%n", DatabaseConstant.NOT_ACCESSIBLE, exception.getMessage());
+        }
+    }
+
+    public Optional<ResultSet> executeQuery(QueryBuilder sql) {
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(sql.toString());
+            return Optional.ofNullable(statement.executeQuery());
+        }
+        catch (SQLTimeoutException exception) {
+            System.err.printf("%s : %s%n", DatabaseConstant.CONNECTION_TIMEOUT, exception.getMessage());
+        }
+        catch (SQLException exception) {
+            System.err.printf("%s : %s", DatabaseConstant.NOT_ACCESSIBLE, exception.getMessage());
+        }
+        return Optional.empty();
+    }
+
+    public void close() {
+        try {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
+        catch (SQLException exception) {
+            System.err.printf("%s : %s%n", DatabaseConstant.CONNECTION_CLOSE_ERROR, exception.getMessage());
+        }
+    }
+}
