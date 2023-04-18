367d4222ba863e52e1e09d852129d40ab34f54f2
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-18 11:02:27 +0100

    Created `DatabaseConnectionTester` singleton class.

---
+++ src/main/java/winchester/library/data/access/DatabaseConnectionTester.java
@@ -0,0 +1,56 @@
+package winchester.library.data.access;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+
+public class DatabaseConnectionTester {
+
+    private static DatabaseConnectionTester instance;
+
+    private DatabaseConnectionTester() { }
+
+    public static DatabaseConnectionTester getInstance() {
+        if (DatabaseConnectionTester.instance == null) {
+            DatabaseConnectionTester.instance = new DatabaseConnectionTester();
+        }
+        return DatabaseConnectionTester.instance;
+    }
+
+    public DatabaseConstant testDriver() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DatabaseConstant.DRIVER_FOUND;
+        }
+        catch (ClassNotFoundException ignored) {
+            return DatabaseConstant.DRIVER_NOT_FOUND;
+        }
+    }
+
+    public DatabaseConstant testCredentials(DatabaseCredentials credentials) {
+        return this.testCredentials(credentials.getUrl(), credentials.getUsername(), credentials.getPassword());
+    }
+
+    public DatabaseConstant testCredentials(String url, String username, String password) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            connection.close();
+            return DatabaseConstant.CONNECTION_SUCCESSFUL;
+        }
+        catch (ClassNotFoundException ignored) {
+            return DatabaseConstant.DRIVER_NOT_FOUND;
+        }
+        catch (SQLTimeoutException ignored) {
+            return DatabaseConstant.CONNECTION_TIMEOUT;
+        }
+        catch (SQLException ignored) {
+            return DatabaseConstant.NOT_ACCESSIBLE;
+        }
+        catch (Exception ignored) {
+            return DatabaseConstant.UNKNOWN_ERROR;
+        }
+    }
+
+}
