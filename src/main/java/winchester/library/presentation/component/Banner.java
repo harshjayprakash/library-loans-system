5935b1b17472d5dadd3ebba0f27f9d650a5562c6
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-19 20:09:34 +0100

    Added controls.

--- src/main/java/winchester/library/presentation/component/Banner.java
+++ src/main/java/winchester/library/presentation/component/Banner.java
@@ -1,15 +1,42 @@
 package winchester.library.presentation.component;
 
-import javafx.scene.layout.BorderPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-public class Banner extends BorderPane {
+public class Banner extends VBox {
 
-    private String message;
-    private String messageDetails;
+    private final String message;
+    private final String messageDetails;
+    private Label messageLabel;
+    private Label messageDetailsLabel;
 
     public Banner(String message, String messageDetails) {
+        super();
         this.message = message;
         this.messageDetails = messageDetails;
+        this.initialiseLayouts();
+        this.initialiseControls();
+        this.addComponentsToBanner();
+    }
+
+    private void initialiseLayouts() {
+        this.setAlignment(Pos.CENTER_LEFT);
+        this.setId("background-secondary");
+        this.setPadding(new Insets(15));
+    }
+
+    private void initialiseControls() {
+        this.messageLabel = new Label();
+        this.messageLabel.setId("text-bold");
+        this.messageLabel.setText(this.message);
+        this.messageDetailsLabel = new Label();
+        this.messageDetailsLabel.setText(this.messageDetails);
+    }
+
+    private void addComponentsToBanner() {
+        this.getChildren().addAll(this.messageLabel, this.messageDetailsLabel);
     }
 
 }
