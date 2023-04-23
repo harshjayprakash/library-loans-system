97af1ab3cb549e8f660dd09f3ad8ccc23ff5d675
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-23 14:56:19 +0100

    Added Controls.

--- src/main/java/winchester/library/presentation/view/CustomerView.java
+++ src/main/java/winchester/library/presentation/view/CustomerView.java
@@ -1,23 +1,64 @@
 package winchester.library.presentation.view;
 
+import java.util.ArrayList;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import winchester.library.data.model.users.Customer;
+import winchester.library.presentation.component.Banner;
+import winchester.library.presentation.component.UserCard;
+import winchester.library.service.DatabaseInteraction;
+
 public class CustomerView extends View {
+
+    private ScrollPane scrollPane;
+    private VBox customerList;
+    private ArrayList<UserCard> customersCardList;
+    private Banner banner;
 
     public CustomerView() {
         super();
+        setSpacing(20);
+        this.initialiseLayouts();
+        this.initialiseControls();
+        this.addComponentsToView();
     }
 
     @Override
     protected void initialiseLayouts() {
-
+        this.customerList = new VBox();
+        this.customerList.setId("background-primary");
+        this.customerList.setSpacing(20);
+        this.scrollPane = new ScrollPane();
+        this.scrollPane.setId("background-primary");
+        this.scrollPane.setFitToWidth(true);
     }
 
     @Override
     protected void initialiseControls() {
-
+        ArrayList<Customer> customers = DatabaseInteraction.getInstance().getCustomers();
+        if (customers.isEmpty()) {
+            this.banner = new Banner(
+                    "No Customers",
+                    ""
+            );
+            return;
+        }
+        this.customersCardList = new ArrayList<>();
+        for (Customer customer : customers) {
+            this.customersCardList.add(new UserCard(customer));
+        }
     }
 
     @Override
     protected void addComponentsToView() {
-
+        if (this.banner != null) {
+            this.getChildren().add(this.banner);
+            return;
+        }
+        for (UserCard card : this.customersCardList) {
+            this.customerList.getChildren().add(card);
+        }
+        this.scrollPane.setContent(this.customerList);
+        this.getChildren().add(this.scrollPane);
     }
 }
