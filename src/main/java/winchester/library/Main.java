2a52eeaf2e2a457f730a22440df129be769ba084
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-24 13:41:42 +0100

    Set Console Printer attributes.

--- src/main/java/winchester/library/Main.java
+++ src/main/java/winchester/library/Main.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 import winchester.library.presentation.style.StylesheetManager;
 import winchester.library.presentation.view.Views;
 import winchester.library.presentation.window.IndividualViewWindow;
+import winchester.library.service.ConsolePrinter;
 
 /**
  * The Main class specifies the entrypoint into the program, inheriting JavaFX Application to provide a start point.
@@ -13,6 +14,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+        ConsolePrinter.getInstance().setExceptionMessagesEnabled(false);
+        ConsolePrinter.getInstance().setEnabled(false);
         StylesheetManager.getInstance().addMultipleStylesheets(
                 "/winchester/library/presentation/style/base.css", "/winchester/library/presentation/style/style.css");
         IndividualViewWindow loginWindow = new IndividualViewWindow(Views.LOGIN);
