e8864ba80ba478b919c7e7c66200d513d43fda48
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-24 12:02:52 +0100

    Added implementation.

--- src/main/java/winchester/library/service/ConsolePrinter.java
+++ src/main/java/winchester/library/service/ConsolePrinter.java
@@ -2,4 +2,29 @@ package winchester.library.service;
 
 public class ConsolePrinter {
 
+    private static ConsolePrinter instance = null;
+
+    private ConsolePrinter() {
+
+    }
+
+    public static ConsolePrinter getInstance() {
+        if (ConsolePrinter.instance == null) {
+            ConsolePrinter.instance = new ConsolePrinter();
+        }
+        return ConsolePrinter.instance;
+    }
+
+    public void WriteLine(String message) {
+        System.out.println(message);
+    }
+
+    public void WriteLineError(String message, String javaMessage) {
+        System.err.printf("%s: %s%n", message, javaMessage);
+    }
+
+    public void WriteLineError(String message) {
+        System.err.println(message);
+    }
+
 }
