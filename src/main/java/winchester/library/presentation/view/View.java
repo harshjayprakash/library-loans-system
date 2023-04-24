dbb72090f73c9e11789c60426d9dc95429ce6e7b
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-24 11:30:17 +0100

    Added window base as required base parameter.

--- src/main/java/winchester/library/presentation/view/View.java
+++ src/main/java/winchester/library/presentation/view/View.java
@@ -2,10 +2,15 @@ package winchester.library.presentation.view;
 
 import javafx.scene.layout.VBox;
 import winchester.library.presentation.style.StylesheetSetter;
+import winchester.library.presentation.window.WindowBase;
 
 public abstract class View extends VBox {
 
-    public View() {
+    protected final WindowBase parentWindow;
+
+    public View(WindowBase parentWindow, String windowTitle) {
+        this.parentWindow = parentWindow;
+        this.parentWindow.setTitleText(windowTitle);
         this.loadStylesheets();
     }
 
