4ffb5e0de652b6a07d5a835a4f1c0988d2010696
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-24 11:36:14 +0100

    Updated status text.

--- src/main/java/winchester/library/data/access/DatabaseStatus.java
+++ src/main/java/winchester/library/data/access/DatabaseStatus.java
@@ -4,8 +4,8 @@ package winchester.library.data.access;
  * A set of values to provide an easy way to show whether the program is connected to the database.
  */
 public enum DatabaseStatus {
-    CONNECTED(1, "Database connected"),
-    NOT_CONNECTED(2, "Database is not connected");
+    CONNECTED(1, "Connected to data source"),
+    NOT_CONNECTED(2, "Data source not found");
 
     private final int identifier;
     private final String value;
