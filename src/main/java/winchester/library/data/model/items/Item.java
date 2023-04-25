e3065963d524db91665ef85df5dd32165f891c2f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-25 10:30:04 +0100

    Added loans to item class.

--- src/main/java/winchester/library/data/model/items/Item.java
+++ src/main/java/winchester/library/data/model/items/Item.java
@@ -1,17 +1,24 @@
 package winchester.library.data.model.items;
 
 import java.util.ArrayList;
+import winchester.library.data.model.loans.Loan;
 
 public abstract class Item {
 
     protected ArrayList<ItemStock> stockAvailable;
+    protected ArrayList<Loan> loans;
 
     public Item() {
         this.stockAvailable = new ArrayList<>();
+        this.loans = new ArrayList<>();
     }
 
     public ArrayList<ItemStock> getStockAvailable() {
         return this.stockAvailable;
+    }
+
+    public ArrayList<Loan> getLoans() {
+        return this.loans;
     }
 
     public abstract ItemType getType();
