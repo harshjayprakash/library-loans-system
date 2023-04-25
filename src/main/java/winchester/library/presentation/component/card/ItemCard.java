6f97ae8d446282e70c18cb38a62a0a00e4f23069
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-25 14:36:26 +0100

    Inherits Card.

--- src/main/java/winchester/library/presentation/component/card/ItemCard.java
+++ src/main/java/winchester/library/presentation/component/card/ItemCard.java
@@ -5,89 +5,81 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import winchester.library.data.model.items.Book;
 import winchester.library.data.model.items.Film;
 import winchester.library.data.model.items.Item;
 import winchester.library.data.model.items.ItemType;
-import winchester.library.presentation.style.StylesheetSetter;
 import winchester.library.presentation.view.Views;
 import winchester.library.presentation.window.IndividualViewWindow;
 
-public class ItemCard extends BorderPane {
+public final class ItemCard extends Card {
 
-    private ImageView itemImage;
-    private VBox itemInformationLayout;
-    private VBox optionsLayout;
+    private ImageView itemImageView;
+    private final int itemImageViewWidth;
+    private final int itemImageViewHeight;
+    private VBox itemInformation;
     private Label itemNameLabel;
-    private Label authorOrDirectorLabel;
+    private Label itemCreatorLabel;
     private Label itemCategoryLabel;
-    private Label detailsLabel;
     private Item item;
 
     public ItemCard(Item item) {
         super();
         this.item = item;
-        this.setId("background-secondary-border");
-        this.setPadding(new Insets(5));
-        StylesheetSetter.getInstance().setStyle(this);
+        this.itemImageViewWidth = 60;
+        this.itemImageViewHeight = 85;
         this.initialiseLayouts();
         this.initialiseControls();
         this.bindEventHandlers();
         this.addComponentsToCard();
     }
 
-    private void initialiseLayouts() {
-        this.itemInformationLayout = new VBox();
-        this.itemInformationLayout.setId("background-secondary");
-        this.itemInformationLayout.setPadding(new Insets(10));
-        this.optionsLayout = new VBox();
-        this.optionsLayout.setId("background-secondary");
-        this.optionsLayout.setPadding(new Insets(10));
+    @Override
+    protected void initialiseLayouts() {
+        this.itemInformation = new VBox();
+        this.itemInformation.setId("background-secondary");
+        this.itemInformation.setPadding(new Insets(10));
     }
 
-    private void initialiseControls() {
-        this.itemImage = new ImageView();
-        this.itemImage.setFitWidth(60);
-        this.itemImage.setFitHeight(85);
+    @Override
+    protected void initialiseControls() {
+        this.itemImageView = new ImageView();
+        this.itemImageView.setFitWidth(this.itemImageViewWidth);
+        this.itemImageView.setFitHeight(this.itemImageViewHeight);
         this.itemNameLabel = new Label();
-        this.itemNameLabel.setText("ItemCard->itemNameLabel");
-        this.authorOrDirectorLabel = new Label();
-        this.authorOrDirectorLabel.setText("ItemCard->authorOrDirectorLabel");
+        this.itemCreatorLabel = new Label();
         this.itemCategoryLabel = new Label();
         this.itemCategoryLabel.setText(this.item.getType().toString());
-        this.detailsLabel = new Label();
-        this.detailsLabel.setId("link-label");
-        this.detailsLabel.setText("View Details");
         if (this.item.getType() == ItemType.BOOK) {
-            Book book = (Book) this.item;
-            this.itemImage.setImage(new Image(book.getImageUrl(), 100, 100, false, false));
-            this.itemNameLabel.setText(book.getTitle());
-            this.authorOrDirectorLabel.setText(book.getAuthor());
+            Book castedBook = Book.castFrom(this.item);
+            this.itemImageView.setImage(new Image(
+                    castedBook.getImageUrl(), this.itemImageViewWidth, this.itemImageViewHeight, false, false));
+            this.itemNameLabel.setText(castedBook.getTitle());
+            this.itemCreatorLabel.setText(castedBook.getAuthor());
         }
-        if (this.item.getType() == ItemType.FILM) {
-            Film film = (Film) this.item;
-            this.itemImage.setImage(new Image(film.getImageUrl(), 100, 100, false, false));
-            this.itemNameLabel.setText(film.getTitle());
-            this.authorOrDirectorLabel.setText(film.getDirector());
+        else if (this.item.getType() == ItemType.FILM) {
+            Film castedFilm = Film.castFrom(this.item);
+            this.itemImageView.setImage(new Image(
+                    castedFilm.getImageUrl(), this.itemImageViewWidth, this.itemImageViewHeight, false, false));
+            this.itemNameLabel.setText(castedFilm.getTitle());
+            this.itemCreatorLabel.setText(castedFilm.getDirector());
         }
     }
 
-    private void bindEventHandlers() {
-        this.detailsLabel.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            IndividualViewWindow itemView = new IndividualViewWindow(Views.INDIVIDUAL_ITEM);
-            itemView.show();
+    @Override
+    protected void bindEventHandlers() {
+        this.viewDetailsLabel.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            IndividualViewWindow individualItemView = new IndividualViewWindow(Views.INDIVIDUAL_ITEM);
+            individualItemView.show();
         });
     }
 
-    private void addComponentsToCard() {
-        this.optionsLayout.getChildren().addAll(this.detailsLabel);
-        this.itemInformationLayout.getChildren().addAll(
-                this.itemNameLabel, this.authorOrDirectorLabel, this.itemCategoryLabel);
-        this.setRight(this.optionsLayout);
-        this.setLeft(this.itemImage);
-        this.setCenter(this.itemInformationLayout);
+    @Override
+    protected void addComponentsToCard() {
+        this.itemInformation.getChildren().addAll(this.itemNameLabel, this.itemCreatorLabel, this.itemCategoryLabel);
+        this.setLeft(this.itemImageView);
+        this.setCenter(this.itemInformation);
     }
 
 }
