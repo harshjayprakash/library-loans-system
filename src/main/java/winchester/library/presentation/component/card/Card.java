3616e3f5d371c4938662b037795b5f784d89e6cb
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-25 13:26:11 +0100

    Created `Card` abstract class.

---
+++ src/main/java/winchester/library/presentation/component/card/Card.java
@@ -0,0 +1,42 @@
+package winchester.library.presentation.component.card;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import winchester.library.presentation.style.StylesheetSetter;
+
+public abstract class Card extends BorderPane {
+
+    protected VBox actionsLayout;
+    protected Label viewDetailsLinkLabel;
+
+    public Card() {
+        this.setId("background-secondary-border");
+        this.loadStylesheets();
+        this.initialiseLayouts();
+        this.initialiseControls();
+        this.addComponentsToCard();
+    }
+
+    private void loadStylesheets() {
+        StylesheetSetter.getInstance().setStyle(this);
+    }
+
+    private void initialiseLayouts() {
+        this.actionsLayout = new VBox();
+    }
+
+    protected void initialiseControls() {
+        this.viewDetailsLinkLabel = new Label();
+        this.viewDetailsLinkLabel.setId("link-label");
+        this.viewDetailsLinkLabel.setText("View Details");
+    }
+
+    protected abstract void bindEventHandlers();
+
+    protected void addComponentsToCard() {
+        this.actionsLayout.getChildren().add(this.viewDetailsLinkLabel);
+        this.setRight(this.actionsLayout);
+    }
+
+}
