cad84475d6f6c3d6fee7268c29f69b0cadf46142
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-26 19:23:56 +0100

    Added file writing operations.

--- src/main/java/winchester/library/data/model/util/Exporter.java
+++ src/main/java/winchester/library/data/model/util/Exporter.java
@@ -8,8 +8,26 @@ public class Exporter {
 
     }
 
-    public boolean export(Exportable entity) {
-        return entity.export();
+    public boolean export(Exportable exportable) {
+        try {
+            LocalDateTime dateTime = LocalDateTime.now();
+            DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH mm ss");
+            FileWriter exportFile = new FileWriter(
+                    String.format("%s/%s WL Export.txt", Exporter.exportOutputPath, dateTime.format(dateTimeFormat)));
+            exportFile.write(String.format(
+                            """
+                            --- Winchester Library Services ---
+                            Exported %s
+                            
+                            %s
+                            
+                            """, dateTime.format(dateTimeFormat), exportable.export()));
+            exportFile.close();
+        }
+        catch (IOException exception) {
+            System.err.println(exception.toString());
+        }
+        return true;
     }
 
     public static String getExportOutputPath() {
