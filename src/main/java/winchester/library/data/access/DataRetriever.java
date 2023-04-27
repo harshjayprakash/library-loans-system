523aade8799775c6c0331f514464bbf22ec28786
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-27 09:22:39 +0100

    Added order by sql operations.

--- src/main/java/winchester/library/data/access/DataRetriever.java
+++ src/main/java/winchester/library/data/access/DataRetriever.java
@@ -92,13 +92,14 @@ public class DataRetriever {
         DatabaseConnection connection = new DatabaseConnection();
         connection.establish(credentials);
         ArrayList<Employee> employees = dataMapper.mapAsEmployee(connection.executeQuery(
-                QueryBuilder.createQuery(QueryType.GET_AND_FILTER)
+                QueryBuilder.createQuery(QueryType.ORDERED_GET_AND_FILTER)
                         .select("employee_id", "first_name", "last_name", "postal_code", "username", "password",
                                 "status_id")
                         .from("library.users", "library.employees", "library.user_types")
                         .where("library.users.user_type_id = library.user_types.user_type_id",
                                 "and library.users.user_id = library.employees.employee_id",
                                 "and library.user_types.user_type <> 'Customer'")
+                        .orderBy("first_name asc", "last_name asc")
         ).orElse(null)).orElse(new ArrayList<>());
         connection.close();
         return employees;
@@ -108,13 +109,14 @@ public class DataRetriever {
         DatabaseConnection connection = new DatabaseConnection();
         connection.establish(credentials);
         ArrayList<Loan> loans = dataMapper.mapAsLoans(connection.executeQuery(
-                QueryBuilder.createQuery(QueryType.GET_AND_FILTER)
+                QueryBuilder.createQuery(QueryType.ORDERED_GET_AND_FILTER)
                         .select("loan_id", "customer_id", "first_name", "last_name", "postal_code", "loans.item_id",
                                 "loans.item_subtype_id", "loan_date", "return_date", "returned")
                         .from("library.loans", "library.users", "library.item_subtypes", "library.item_copies")
                         .where("library.loans.customer_id = library.users.user_id",
                                 "and library.loans.item_id = library.item_copies.item_id",
                                 "and library.loans.item_subtype_id = library.item_subtypes.item_subtype_id")
+                        .orderBy("loan_date asc")
         ).orElse(null)).orElse(new ArrayList<>());
         connection.close();
         return loans;
