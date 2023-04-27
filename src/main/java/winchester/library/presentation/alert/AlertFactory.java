1af604483008f731f28a1087337c0bb3b22ff1c5
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-27 12:42:14 +0100

    Created `AlertFactory` class.

---
+++ src/main/java/winchester/library/presentation/alert/AlertFactory.java
@@ -0,0 +1,14 @@
+package winchester.library.presentation.alert;
+
+import javafx.scene.control.Alert;
+
+public final class AlertFactory {
+
+    public static Alert createAlert(Alert.AlertType type, String headerText, String contextText) {
+        Alert alert = new Alert(type);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contextText);
+        return alert;
+    }
+
+}
