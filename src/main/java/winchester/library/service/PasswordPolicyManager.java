579cab09eda8b35eda0648bebfa88b5d812c7e42
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-27 10:06:51 +0100

    Created `PasswordPolicyManager` class.

---
+++ src/main/java/winchester/library/service/PasswordPolicyManager.java
@@ -0,0 +1,37 @@
+package winchester.library.service;
+
+public class PasswordPolicyManager {
+
+    public final String passwordRequirements;
+
+    public PasswordPolicyManager() {
+        this.passwordRequirements =
+                """
+                Your password must:
+                - be at least 8 characters long
+                - contain a number (0-9)
+                - contain a letter (A-Z, a-z)
+                - contain a special character (. , : @ # £ $ % & * ! ? < > SPACE)
+                """;
+    }
+
+    public String getPasswordRequirements() {
+        return this.passwordRequirements;
+    }
+
+    public boolean checkPassword() {
+        return false;
+    }
+
+    private boolean checkLength() {
+        return false;
+    }
+
+    private boolean checkForNumber() {
+        return false;
+    }
+
+    private boolean checkForSpecialCharacter() {
+        return false;
+    }
+}
