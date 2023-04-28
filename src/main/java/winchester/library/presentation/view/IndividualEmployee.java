911c34f711822bb6a325948d026340085933d025
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-28 11:36:38 +0100

    Created `IndividualEmployee` class.

---
+++ src/main/java/winchester/library/presentation/view/IndividualEmployee.java
@@ -0,0 +1,25 @@
+package winchester.library.presentation.view;
+
+import winchester.library.presentation.window.WindowBase;
+
+public class IndividualEmployee extends View {
+
+    public IndividualEmployee(WindowBase parentWindow) {
+        super(parentWindow, Views.INDIVIDUAL_EMPLOYEE.toString());
+    }
+
+    @Override
+    protected void initialiseLayouts() {
+
+    }
+
+    @Override
+    protected void initialiseControls() {
+
+    }
+
+    @Override
+    protected void addComponentsToView() {
+
+    }
+}
