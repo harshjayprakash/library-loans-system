e361715430258c6870a48ef93a78d6a65d8cead5
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-29 17:06:57 +0100

    Added insert methods.

--- src/main/java/winchester/library/data/access/DataWriter.java
+++ src/main/java/winchester/library/data/access/DataWriter.java
@@ -1,11 +1,39 @@
 package winchester.library.data.access;
 
+import winchester.library.data.model.items.Book;
+import winchester.library.data.model.items.Film;
+import winchester.library.data.model.users.Administrator;
+import winchester.library.data.model.users.Customer;
+import winchester.library.data.model.users.Employee;
+
+import java.util.function.Function;
+
 /**
  * A class that abstracts the method of creating a new record within the database.
  */
 public class DataWriter {
 
     public DataWriter() {
+
+    }
+
+    public void insert(Book book) {
+
+    }
+
+    public void insert(Film film) {
+
+    }
+
+    public void insert(Customer customer) {
+
+    }
+
+    public void insert(Employee employee) {
+
+    }
+
+    public void insert(Administrator administrator) {
 
     }
 
