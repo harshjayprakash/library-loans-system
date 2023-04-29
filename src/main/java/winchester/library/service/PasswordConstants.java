f2f2d7c3f7206d6899ddf2a1ab12e5124a0aca3f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-29 17:58:01 +0100

    Created `PasswordConstants` enum.

---
+++ src/main/java/winchester/library/service/PasswordConstants.java
@@ -0,0 +1,39 @@
+package winchester.library.service;
+
+import java.util.Optional;
+
+public enum PasswordConstants {
+    REQUIREMENTS_MET(0, "Password requirements have been met"),
+    NOT_LONG_ENOUGH(1, "not long enough"),
+    DOES_NOT_CONTAIN_NUMBER(2, "does not contain a number"),
+    DOES_NOT_CONTAIN_LETTER(3, "does not contain a letter"),
+    DOES_NOT_CONTAIN_SPECIAL_CHARACTER(4, "does not contain a special character"),
+    DOES_NOT_MEET_ANY_REQUIREMENTS(10, "does not meet any of the requirements");
+
+
+    private final int identifier;
+    private final String message;
+
+    PasswordConstants(int identifier, String message) {
+        this.identifier = identifier;
+        this.message = message;
+    }
+
+    public static Optional<PasswordConstants> fromIdentifier(int identifier) {
+        for (PasswordConstants constant : PasswordConstants.values()) {
+            if (constant.identifier == identifier) {
+                return Optional.of(constant);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public int getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
+}
