d473e5896219ea40af8b60a683fc352bf8639a5f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-04-30 17:11:34 +0100

    Added insert functions from employee types.

--- src/main/java/winchester/library/data/access/DataWriter.java
+++ src/main/java/winchester/library/data/access/DataWriter.java
@@ -1,12 +1,11 @@
 package winchester.library.data.access;
 
+import java.util.Optional;
 import winchester.library.data.model.items.Book;
 import winchester.library.data.model.items.Film;
 import winchester.library.data.model.users.Administrator;
 import winchester.library.data.model.users.Customer;
 import winchester.library.data.model.users.Employee;
-
-import java.util.function.Function;
 
 /**
  * A class that abstracts the method of creating a new record within the database.
@@ -31,12 +30,56 @@ public class DataWriter {
 
     }
 
-    public void insert(Employee employee) {
-
+    public DatabaseConstant insert(Employee employee) {
+        DatabaseConnection connection = new DatabaseConnection();
+        connection.establish(this.credentials);
+        Optional<Integer> userInsertResult = connection.executeUpdate(
+                QueryBuilder.createQuery(QueryType.INSERT_ONE)
+                        .insertInto("library.users")
+                        .values(String.format("(%d, %d, '%s', '%s', '%s')",
+                                employee.getIdentifier(), employee.getType().getIdentifier(), employee.getFirstName(),
+                                employee.getLastName(), employee.getPostalCode())));
+        if (userInsertResult.isEmpty()) {
+            return DatabaseConstant.INSERTION_ERROR;
+        }
+        Optional<Integer> employeeInsertResult = connection.executeUpdate(
+                QueryBuilder.createQuery(QueryType.INSERT_ONE)
+                        .insertInto("library.employees (user_id, username, password, status_id)")
+                        .values(String.format("(%d, '%s', '%s', %d)",
+                                employee.getIdentifier(), employee.getUsername(), employee.getPassword(),
+                                employee.getStatus().getIdentifier()))
+        );
+        if (employeeInsertResult.isEmpty()) {
+            return DatabaseConstant.INSERTION_ERROR;
+        }
+        connection.close();
+        return DatabaseConstant.INSERTION_SUCCESSFUL;
     }
 
-    public void insert(Administrator administrator) {
-
+    public DatabaseConstant insert(Administrator administrator) {
+        DatabaseConnection connection = new DatabaseConnection();
+        connection.establish(this.credentials);
+        Optional<Integer> userInsertResult = connection.executeUpdate(
+                QueryBuilder.createQuery(QueryType.INSERT_ONE)
+                        .insertInto("library.users")
+                        .values(String.format("(%d, %d, %s, %s, %s)",
+                                administrator.getIdentifier(), administrator.getType().getIdentifier(), administrator.getFirstName(),
+                                administrator.getLastName(), administrator.getPostalCode())));
+        if (userInsertResult.isEmpty()) {
+            return DatabaseConstant.INSERTION_ERROR;
+        }
+        Optional<Integer> employeeInsertResult = connection.executeUpdate(
+                QueryBuilder.createQuery(QueryType.INSERT_ONE)
+                        .insertInto("library.employees (user_id, username, password, status_id)")
+                        .values(String.format("(%d, %s, %s, %d)",
+                                administrator.getIdentifier(), administrator.getUsername(), administrator.getPassword(),
+                                administrator.getStatus().getIdentifier()))
+        );
+        if (employeeInsertResult.isEmpty()) {
+            return DatabaseConstant.INSERTION_ERROR;
+        }
+        connection.close();
+        return DatabaseConstant.INSERTION_SUCCESSFUL;
     }
 
 }
