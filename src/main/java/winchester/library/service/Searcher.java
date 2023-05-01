1fd5747a5930e46da990945ba790890de05bcf92
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-01 13:10:41 +0100

    Moved package.

---
+++ src/main/java/winchester/library/service/Searcher.java
@@ -0,0 +1,15 @@
+package winchester.library.service;
+
+import java.util.concurrent.atomic.AtomicReference;
+import winchester.library.data.model.items.Item;
+
+/**
+ * A class that searches through lists of entities.
+ */
+public class Searcher {
+
+    public Searcher() {
+
+    }
+
+}
