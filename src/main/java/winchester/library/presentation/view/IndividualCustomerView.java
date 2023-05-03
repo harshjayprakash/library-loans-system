ac21d517593c6204a1c14f2dee68a903f858a36c
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-03 10:24:24 +0100

    Added customer parameter.

--- src/main/java/winchester/library/presentation/view/IndividualCustomerView.java
+++ src/main/java/winchester/library/presentation/view/IndividualCustomerView.java
@@ -1,10 +1,11 @@
 package winchester.library.presentation.view;
 
+import winchester.library.data.model.users.Customer;
 import winchester.library.presentation.window.WindowBase;
 
 public final class IndividualCustomerView extends View {
 
-    public IndividualCustomerView(WindowBase parentWindow) {
+    public IndividualCustomerView(WindowBase parentWindow, Customer customer) {
         super(parentWindow, Views.INDIVIDUAL_CUSTOMER.toString());
     }
 
