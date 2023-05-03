b8ef74f3cb32a797c3768223d6cb812f4a3129f8
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-03 10:23:26 +0100

    Removed type argument and Loan Management interface.

--- src/main/java/winchester/library/data/model/users/Employee.java
+++ src/main/java/winchester/library/data/model/users/Employee.java
@@ -1,22 +1,21 @@
 package winchester.library.data.model.users;
 
 import winchester.library.data.access.DatabaseEntity;
-import winchester.library.data.model.loans.LoanManagement;
 
 /**
  * A class to model an employee of the system.
  */
 @DatabaseEntity(table = "employees")
-public class Employee extends User implements LoanManagement {
+public class Employee extends User {
     
     private final String username;
     private String password;
     private EmployeeStatus status;
 
-    public Employee(int identifier, UserType type, String firstName, String lastName, String postalCode, String username,
+    public Employee(int identifier, String firstName, String lastName, String postalCode, String username,
                     String password, EmployeeStatus status) {
         super(identifier, firstName, lastName, postalCode);
-        this.type = type;
+        this.type = UserType.STANDARD;
         this.username = username;
         this.password = password;
         this.status = status;
@@ -52,24 +51,7 @@ public class Employee extends User implements LoanManagement {
                 %s
                 Type: %s
                 Username: %s
-                Password: %s
                 Status: %s
-               """.formatted(super.toString(), this.getType().toString(), this.username, this.password,
-                this.status.toString());
-    }
-
-    @Override
-    public void loanItem() {
-
-    }
-
-    @Override
-    public void returnItem() {
-
-    }
-
-    @Override
-    public void extendItem() {
-
+               """.formatted(super.toString(), this.getType().toString(), this.username, this.status.toString());
     }
 }
