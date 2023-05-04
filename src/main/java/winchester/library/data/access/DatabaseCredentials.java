ae6a15c06fe03ab9ac36cf7b4c02a2d1d4d709c8
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-04 15:56:08 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/data/access/DatabaseCredentials.java
+++ src/main/java/winchester/library/data/access/DatabaseCredentials.java
@@ -2,19 +2,24 @@ package winchester.library.data.access;
 
 /**
  * A singleton class that stores the credentials to connect to the database, providing mutators for the ability of
- * changing the credentials
+ * changing the credentials. This class as been marked as final to prevent extension.
  */
-public class DatabaseCredentials {
+public final class DatabaseCredentials {
 
     private static DatabaseCredentials instance;
     private String url = "jdbc:mysql://localhost:3306/library";
     private String username = "root";
     private String password = "dbpassword";
 
-    private DatabaseCredentials() {
+    /**
+     * The default constructor.
+     */
+    private DatabaseCredentials() { }
 
-    }
-
+    /**
+     * This is a method to get the singleton instance of the DatabaseCredentials class.
+     * @return the single instance of the class.
+     */
     public static DatabaseCredentials getInstance() {
         if (DatabaseCredentials.instance == null) {
             DatabaseCredentials.instance = new DatabaseCredentials();
@@ -22,26 +27,50 @@ public class DatabaseCredentials {
         return DatabaseCredentials.instance;
     }
 
+    /**
+     * An accessor that retrieves the database url.
+     * @return the database URL as a String.
+     */
     public String getUrl() {
         return this.url;
     }
 
+    /**
+     * A mutator that sets the database url.
+     * @param url the database url.
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * An accessor that retrieves the database username.
+     * @return the database username.
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * A mutator that sets the database username.
+     * @param username the database username.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * An accessor that retrieves the database password.
+     * @return the database password.
+     */
     public String getPassword() {
         return this.password;
     }
 
+    /**
+     * A mutator that sets the database password.
+     * @param password the database password.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
