a818bc74433bd5c09d8403773fa3082c3dc4dad2
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-04 15:55:15 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/data/access/DatabaseConnectionTester.java
+++ src/main/java/winchester/library/data/access/DatabaseConnectionTester.java
@@ -6,14 +6,20 @@ import java.sql.SQLException;
 import java.sql.SQLTimeoutException;
 
 /**
- * A class that provides an interface for testing the database connection credentials, providing a status.
+ * A class that provides an interface for testing the database connection credentials, providing a status. This class
+ * has been marked as final to prevent extension.
  */
-public class DatabaseConnectionTester {
+public final class DatabaseConnectionTester {
 
-    public DatabaseConnectionTester() {
+    /**
+     * The default constructor.
+     */
+    public DatabaseConnectionTester() { }
 
-    }
-
+    /**
+     * A method to test whether the MySQL JDBC Driver is available.
+     * @return a DatabaseConstant enumeration that specifies if the driver was found.
+     */
     public DatabaseConstant testDriver() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -24,10 +30,22 @@ public class DatabaseConnectionTester {
         }
     }
 
+    /**
+     * A method to test whether a connection can be established with the provided credentials.
+     * @param credentials an instance of the DatabaseCredentials class that stores the url, username and password.
+     * @return a DatabaseConstant enumeration that specifies the connection was successful.
+     */
     public DatabaseConstant testCredentials(DatabaseCredentials credentials) {
         return this.testCredentials(credentials.getUrl(), credentials.getUsername(), credentials.getPassword());
     }
 
+    /**
+     * A method to test whether a connection can be established with a provided url, username and password.
+     * @param url the database url.
+     * @param username the database username.
+     * @param password the database password.
+     * @return a DatabaseConstant enumeration that specifies if the connection was successful.
+     */
     public DatabaseConstant testCredentials(String url, String username, String password) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
