37516fbc37483c9986022bd07db170415cb1c435
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-05 08:33:24 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/data/access/DatabaseEntity.java
+++ src/main/java/winchester/library/data/access/DatabaseEntity.java
@@ -6,10 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * A method of marking database entities with their corresponding tables in the database. This can only annotate types.
+ * A method of marking database entities with their corresponding tables in the database.
+ * This can only annotate types.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 public @interface DatabaseEntity {
+    /**
+     * An accessor to get the tables specified.
+     * @return the tables specified.
+     */
     String table();
 }
