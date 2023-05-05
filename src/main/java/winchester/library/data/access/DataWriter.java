7f613d9a472a035f790489e681d3ac47cc8eb891
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-05 08:43:31 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/data/access/DataWriter.java
+++ src/main/java/winchester/library/data/access/DataWriter.java
@@ -12,10 +12,19 @@ public class DataWriter {
 
     private final DatabaseCredentials credentials;
 
+    /**
+     * The default constructor that gets the DatabaseCredentials instance.
+     */
     public DataWriter() {
         this.credentials = DatabaseCredentials.getInstance();
     }
 
+    /**
+     * A method to insert an employee into the data source.
+     * @param employee the employee to be inserted.
+     * @return a DatabaseConstant enumeration of either INSERTION_ERROR if the operation failed or if the operation
+     * succeeded returns it returns INSERTION_SUCCESSFUL.
+     */
     public DatabaseConstant insert(Employee employee) {
         DatabaseConnection connection = new DatabaseConnection();
         connection.establish(this.credentials);
@@ -42,32 +51,12 @@ public class DataWriter {
         return DatabaseConstant.INSERTION_SUCCESSFUL;
     }
 
-    public DatabaseConstant insert(Administrator administrator) {
-        DatabaseConnection connection = new DatabaseConnection();
-        connection.establish(this.credentials);
-        Optional<Integer> userInsertResult = connection.executeUpdate(
-                QueryBuilder.createQuery(QueryType.INSERT_ONE)
-                        .insertInto("library.users")
-                        .values(String.format("(%d, %d, '%s', '%s', '%s')",
-                                administrator.getIdentifier(), administrator.getType().getIdentifier(), administrator.getFirstName(),
-                                administrator.getLastName(), administrator.getPostalCode())));
-        if (userInsertResult.isEmpty()) {
-            return DatabaseConstant.INSERTION_ERROR;
-        }
-        Optional<Integer> employeeInsertResult = connection.executeUpdate(
-                QueryBuilder.createQuery(QueryType.INSERT_ONE)
-                        .insertInto("library.employees (user_id, username, password, status_id)")
-                        .values(String.format("(%d, '%s', '%s', %d)",
-                                administrator.getIdentifier(), administrator.getUsername(), administrator.getPassword(),
-                                administrator.getStatus().getIdentifier()))
-        );
-        if (employeeInsertResult.isEmpty()) {
-            return DatabaseConstant.INSERTION_ERROR;
-        }
-        connection.close();
-        return DatabaseConstant.INSERTION_SUCCESSFUL;
-    }
-
+    /**
+     * A method to insert a customer into the data source.
+     * @param customer the customer to be inserted.
+     * @return a DatabaseConstant enumeration of either INSERTION_ERROR if the operation failed or if the operation
+     * succeeded returns it returns INSERTION_SUCCESSFUL.
+     */
     public DatabaseConstant insert(Customer customer) {
         DatabaseConnection connection = new DatabaseConnection();
         connection.establish(this.credentials);
