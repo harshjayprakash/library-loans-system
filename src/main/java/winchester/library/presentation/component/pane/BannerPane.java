ada909136f46d9964f2a78b0c892239cbd913bfd
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 16:56:41 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/presentation/component/pane/BannerPane.java
+++ src/main/java/winchester/library/presentation/component/pane/BannerPane.java
@@ -5,6 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
+/**
+ * A class to provide a banner component to display a message on a window.
+ */
 public final class BannerPane extends VBox {
 
     private final String message;
@@ -12,21 +15,32 @@ public final class BannerPane extends VBox {
     private Label messageLabel;
     private Label messageDetailsLabel;
 
+    /**
+     * The default constructor to set the message to be shown.
+     * @param message the main short message.
+     * @param messageDetails more details on the situation.
+     */
     public BannerPane(String message, String messageDetails) {
         super();
         this.message = message;
         this.messageDetails = messageDetails;
-        this.initialiseLayouts();
+        this.initialiseLayout();
         this.initialiseControls();
         this.addComponentsToPane();
     }
 
-    private void initialiseLayouts() {
+    /**
+     * A method to initialise the layout used with this component.
+     */
+    private void initialiseLayout() {
         this.setAlignment(Pos.CENTER_LEFT);
         this.getStyleClass().add("background-secondary-border");
         this.setPadding(new Insets(10));
     }
 
+    /**
+     * A method to initialise the controls used within this component.
+     */
     private void initialiseControls() {
         this.messageLabel = new Label();
         this.messageLabel.getStyleClass().add("text-bold");
@@ -36,6 +50,9 @@ public final class BannerPane extends VBox {
         this.messageDetailsLabel.setWrapText(true);
     }
 
+    /**
+     * A method to add the components to the banner component.
+     */
     private void addComponentsToPane() {
         this.getChildren().addAll(this.messageLabel, this.messageDetailsLabel);
     }
