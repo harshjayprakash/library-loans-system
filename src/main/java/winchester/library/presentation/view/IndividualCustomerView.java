aa0bf2d67b7a97933359b305de3d6a0b33aa7104
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 17:09:14 +0100

    Added loans ability.

--- src/main/java/winchester/library/presentation/view/IndividualCustomerView.java
+++ src/main/java/winchester/library/presentation/view/IndividualCustomerView.java
@@ -3,11 +3,14 @@ package winchester.library.presentation.view;
 import java.util.ArrayList;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import winchester.library.data.model.loans.Loan;
 import winchester.library.data.model.users.Customer;
 import winchester.library.presentation.component.pane.ActionPane;
+import winchester.library.presentation.window.IndividualViewWindow;
 import winchester.library.presentation.window.WindowBase;
 import winchester.library.service.DataPersistenceManager;
 
@@ -17,6 +20,8 @@ public final class IndividualCustomerView extends View {
     private VBox customerInformationLayout;
     private Text customerInformation;
     private Button addLoansButton;
+    private VBox customerLoansLayoutContainer;
+    private GridPane customerLoansLayout;
     private final Customer referencedCustomer;
 
     public IndividualCustomerView(WindowBase parentWindow, Customer customer) {
@@ -24,6 +29,7 @@ public final class IndividualCustomerView extends View {
         this.referencedCustomer = customer;
         this.initialiseLayouts();
         this.initialiseControls();
+        this.bindEventHandlers();
         this.addComponentsToView();
     }
 
@@ -35,6 +41,10 @@ public final class IndividualCustomerView extends View {
         this.customerInformationLayout.getStyleClass().add("background-secondary-border");
         this.customerInformationLayout.setSpacing(10);
         this.customerInformationLayout.setPadding(new Insets(10));
+        this.customerLoansLayoutContainer = new VBox();
+        this.customerLoansLayoutContainer.getStyleClass().add("background-secondary-border");
+        this.customerLoansLayoutContainer.setPadding(new Insets(10));
+        this.customerLoansLayout = new GridPane();
     }
 
     @Override
@@ -43,15 +53,23 @@ public final class IndividualCustomerView extends View {
         this.customerInformation.setText(this.referencedCustomer.toString());
         this.addLoansButton = new Button();
         this.addLoansButton.setText("Add Loans");
-
         ArrayList<Loan> loans = DataPersistenceManager.getInstance().getLoans();
+        if (loans.isEmpty()) { return; }
+    }
+    private void bindEventHandlers() {
+        this.addLoansButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            IndividualViewWindow loaningView = new IndividualViewWindow(Views.LOANING_ITEMS, this.referencedCustomer);
+            loaningView.show();
+        });
     }
 
     @Override
     protected void addComponentsToView() {
         this.actionsLayout.getLeftControls().getChildren().add(this.addLoansButton);
         this.customerInformationLayout.getChildren().add(this.customerInformation);
-        this.getChildren().addAll(this.actionsLayout, this.customerInformationLayout);
+        this.customerLoansLayoutContainer.getChildren().add(this.customerLoansLayout);
+        this.getChildren().addAll(this.actionsLayout, this.customerInformationLayout,
+                this.customerLoansLayoutContainer);
     }
 
 }
