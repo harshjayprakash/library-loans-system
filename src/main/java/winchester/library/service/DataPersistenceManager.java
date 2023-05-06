4fef881d4ede75bfdb25cb7ca593d7378831bd07
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 17:05:35 +0100

    Added data updater and loan creator.

--- src/main/java/winchester/library/service/DataPersistenceManager.java
+++ src/main/java/winchester/library/service/DataPersistenceManager.java
@@ -1,11 +1,15 @@
 package winchester.library.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import winchester.library.data.access.DataFetcher;
+import winchester.library.data.access.DataUpdater;
 import winchester.library.data.access.DataWriter;
 import winchester.library.data.access.DatabaseConstant;
 import winchester.library.data.model.items.Book;
 import winchester.library.data.model.items.Film;
+import winchester.library.data.model.items.Item;
+import winchester.library.data.model.items.ItemFormat;
 import winchester.library.data.model.loans.Loan;
 import winchester.library.data.model.users.Administrator;
 import winchester.library.data.model.users.Customer;
@@ -14,19 +18,28 @@ import winchester.library.data.model.users.EmployeeStatus;
 import winchester.library.data.model.users.UserType;
 
 /**
- * A class that provides an abstraction to the database operations for the graphical user interface.
+ * A singleton class that provides an abstraction to the database operations for the graphical user interface.
  */
 public final class DataPersistenceManager {
 
     private static DataPersistenceManager instance;
     private final DataFetcher dataRetriever;
     private final DataWriter dataWriter;
+    private final DataUpdater dataUpdater;
 
+    /**
+     * A private constructor to prevent multiple instances.
+     */
     private DataPersistenceManager() {
         this.dataRetriever = new DataFetcher();
         this.dataWriter = new DataWriter();
+        this.dataUpdater = new DataUpdater();
     }
 
+    /**
+     * A static accessor to retrieve the single instance of the DataPersistenceManager class.
+     * @return the instance of the class.
+     */
     public static DataPersistenceManager getInstance() {
         if (DataPersistenceManager.instance == null) {
             DataPersistenceManager.instance = new DataPersistenceManager();
@@ -34,36 +47,121 @@ public final class DataPersistenceManager {
         return DataPersistenceManager.instance;
     }
 
+    /**
+     * An accessor to retrieve the list of books from the data source.
+     * @return an array list of books.
+     */
     public ArrayList<Book> getBooks() {
         return dataRetriever.getBooks();
     }
 
+    /**
+     * An accessor to retrieve the list of films from the data source.
+     * @return an array list of film.
+     */
     public ArrayList<Film> getFilms() {
         return dataRetriever.getFilms();
     }
 
+    /**
+     * An accessor to retrieve the list of customers from the data source.
+     * @return an array list of customers.
+     */
     public ArrayList<Customer> getCustomers() {
         return dataRetriever.getCustomers();
     }
 
+    /**
+     * An accessor to retrieve the list of employees from the data source.
+     * @return an array list of employees
+     */
     public ArrayList<Employee> getEmployees() {
         return dataRetriever.getEmployees();
     }
 
+    /**
+     * An accessor to retrieve the list of loans from the data source.
+     * @return an array list of the loans.
+     */
     public ArrayList<Loan> getLoans() {
         return dataRetriever.getLoans();
     }
 
-    public boolean createUser(UserType userType, String firstName, String lastName, String postalCode, String username, String password) {
+    /**
+     * A method to create a new user, syncing it to the data source.
+     * @param userType the user type.
+     * @param firstName the first name of the user.
+     * @param lastName the last name of the user.
+     * @param postalCode the postal code of the user.
+     * @param username the username of the user.
+     * @param password the password of the user.
+     * @return true if the operation was successful.
+     */
+    public boolean createUser(UserType userType, String firstName, String lastName, String postalCode, String username,
+                              String password) {
         return switch (userType) {
-            case STANDARD -> dataWriter.insert(new Employee(new IdentifierGenerator().generateForUser(), firstName, lastName, postalCode, username, password, EmployeeStatus.NOT_APPROVED));
-            case ADMINISTRATOR -> dataWriter.insert(new Administrator(new IdentifierGenerator().generateForUser(), firstName, lastName, postalCode, username, password, EmployeeStatus.NOT_APPROVED));
+            case STANDARD -> dataWriter.insert(new Employee(new IdentifierGenerator().generateForUser(),
+                    firstName, lastName, postalCode, username, password, EmployeeStatus.NOT_APPROVED));
+            case ADMINISTRATOR -> dataWriter.insert(new Administrator(new IdentifierGenerator().generateForUser(),
+                    firstName, lastName, postalCode, username, password, EmployeeStatus.NOT_APPROVED));
             default -> DatabaseConstant.INSERTION_ERROR;
         } == DatabaseConstant.INSERTION_SUCCESSFUL;
     }
 
+    /**
+     * A method to create a new customer, syncing it to the data source.
+     * @param firstName the first name of the customer.
+     * @param lastName the last name of the customer.
+     * @param postalCode the postal code of the customer.
+     * @return true if the operation was successful.
+     */
     public boolean createCustomer(String firstName, String lastName, String postalCode) {
-        return dataWriter.insert(new Customer(new IdentifierGenerator().generateForUser(), firstName, lastName, postalCode)) == DatabaseConstant.INSERTION_SUCCESSFUL;
+        return dataWriter.insert(new Customer(new IdentifierGenerator().generateForUser(),
+                firstName, lastName, postalCode)) == DatabaseConstant.INSERTION_SUCCESSFUL;
+    }
+
+    /**
+     * A method to create a new loan, syncing it to the data source.
+     * @param item the item to be loaned out.
+     * @param format the format of the item to be loaned out.
+     * @param customer the customer loaning out the item.
+     * @return true if the operation was successful
+     */
+    public boolean createLoan(Item item, ItemFormat format, Customer customer) {
+        return switch (item.getType()) {
+            case BOOK -> dataWriter.insert(new Loan(0, customer, Book.castFrom(item).getIsbn(), format,
+                    LocalDate.now(), LocalDate.now().plusDays(30), false));
+            case FILM -> dataWriter.insert(new Loan(0, customer, Film.castFrom(item).getIdentifier(), format,
+                    LocalDate.now(), LocalDate.now().plusDays(10), false));
+        } == DatabaseConstant.INSERTION_SUCCESSFUL;
+    }
+
+    /**
+     * A method to approve an employee, syncing this to the data source.
+     * @param employee the employee to be approved.
+     * @return true if the operation was successful.
+     */
+    public boolean approveEmployee(Employee employee) {
+        return dataUpdater.updateEmployeeStatus(employee, EmployeeStatus.ACTIVE) == DatabaseConstant.UPDATE_SUCCESSFUL;
+    }
+
+    /**
+     * A method to disable an employee, syncing this to the data source.
+     * @param employee the employee to be disabled.
+     * @return true if the operation was successful.
+     */
+    public boolean disableEmployee(Employee employee) {
+        return dataUpdater.updateEmployeeStatus(
+                employee, EmployeeStatus.DISABLED) == DatabaseConstant.UPDATE_SUCCESSFUL;
+    }
+
+    /**
+     * A method to enable an employee, syncing this to the data source.
+     * @param employee the employee to be enabled.
+     * @return true if the operation was successful.
+     */
+    public boolean enableEmployee(Employee employee) {
+        return dataUpdater.updateEmployeeStatus(employee, EmployeeStatus.ACTIVE) == DatabaseConstant.UPDATE_SUCCESSFUL;
     }
 
 }
