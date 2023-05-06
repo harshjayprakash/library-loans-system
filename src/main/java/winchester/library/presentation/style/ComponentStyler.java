2b9ddef94db85356af12d8c3a32aef05571cbc5f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 17:01:55 +0100

    Add Java Documentation.

--- src/main/java/winchester/library/presentation/style/ComponentStyler.java
+++ src/main/java/winchester/library/presentation/style/ComponentStyler.java
@@ -5,14 +5,21 @@ import javafx.scene.Scene;
 import winchester.library.service.Logger;
 
 /**
- * A class that provides abstraction to add stylesheets to a component.
+ * A singleton class that provides abstraction to add stylesheets to a component.
  */
 public final class ComponentStyler {
 
     private static ComponentStyler instance;
 
+    /**
+     * A private constructor to prevent multiple instances.
+     */
     private ComponentStyler() { }
 
+    /**
+     * A static accessor to retrieve the single instance of the ComponentStyler class.
+     * @return an instance of the class.
+     */
     public static ComponentStyler getInstance() {
         if (ComponentStyler.instance == null) {
             ComponentStyler.instance = new ComponentStyler();
@@ -20,6 +27,10 @@ public final class ComponentStyler {
         return ComponentStyler.instance;
     }
 
+    /**
+     * A method to set add the stylesheets to the given component.
+     * @param scene the scene to be styled.
+     */
     public void setStyle(Scene scene) {
         for (String path : StylesheetRegistry.getInstance().getStylesheetPaths()) {
             try {
@@ -42,6 +53,10 @@ public final class ComponentStyler {
         }
     }
 
+    /**
+     * A method to set add the stylesheets to the given component.
+     * @param parent the parent component to be styled.
+     */
     public void setStyle(Parent parent) {
         for (String path : StylesheetRegistry.getInstance().getStylesheetPaths()) {
             try {
