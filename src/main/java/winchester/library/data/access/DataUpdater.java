f126551514ea540b2f4e1ebcaa50fb4e11001bca
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 17:06:08 +0100

    Added `DataUpdater` implementation.

--- src/main/java/winchester/library/data/access/DataUpdater.java
+++ src/main/java/winchester/library/data/access/DataUpdater.java
@@ -1,15 +1,36 @@
 package winchester.library.data.access;
 
+import java.util.Optional;
+import winchester.library.data.model.users.Employee;
+import winchester.library.data.model.users.EmployeeStatus;
+
 /**
  * A class that abstracts the method of updating a record within the database.
  */
 public class DataUpdater {
 
+    private final DatabaseCredentials credentials;
+
     /**
      * The default constructor.
      */
     public DataUpdater() {
+        this.credentials = DatabaseCredentials.getInstance();
+    }
 
+    public DatabaseConstant updateEmployeeStatus(Employee employee, EmployeeStatus status) {
+        DatabaseConnection connection = new DatabaseConnection();
+        connection.establish(this.credentials);
+        Optional<Integer> updateResult = connection.executeUpdate(
+                QueryBuilder.createQuery(QueryType.UPDATE_AND_FILTER)
+                        .update("library.employees")
+                        .set(String.format("status_id = %d", status.getIdentifier()))
+                        .where(String.format("user_id = %d", employee.getIdentifier())));
+        if (updateResult.isEmpty()) {
+            return DatabaseConstant.UPDATE_ERROR;
+        }
+        connection.close();
+        return DatabaseConstant.UPDATE_SUCCESSFUL;
     }
 
 }
