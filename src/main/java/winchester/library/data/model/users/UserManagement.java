ca7db7fdc884d5f77ab72d042d9eba0b5f37f68b
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 16:54:05 +0100

    Deleted `UserManagement` interface.

--- src/main/java/winchester/library/data/model/users/UserManagement.java
+++
@@ -1,9 +0,0 @@
-package winchester.library.data.model.users;
-
-/**
- * An interface to allow user management within the system.
- */
-public interface UserManagement {
-    void approveUser(Employee employee);
-    void disableUser(Employee employee);
-}
