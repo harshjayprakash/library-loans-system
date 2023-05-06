6d9006681e4d82ace5671938e1da3f4858152f28
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-06 16:55:00 +0100

    Added Java Documentation.

--- src/main/java/winchester/library/presentation/component/pane/ActionPane.java
+++ src/main/java/winchester/library/presentation/component/pane/ActionPane.java
@@ -5,27 +5,42 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
 /**
- * A class that provides an abstraction for the styling of the actions panel.
+ * A class that provides an abstraction for the styling and handling controls in the left and right in the BorderPane
+ * layout.
  */
 public class ActionPane extends BorderPane {
 
     HBox leftControls;
     HBox rightControls;
 
+    /**
+     * The default constructor to create the ActionPane.
+     */
     public ActionPane() {
         super();
         this.initialiseLayouts();
-        this.addComponentsToPane();
+        this.addLayoutsToPane();
     }
 
+    /**
+     * An accessor to retrieve the horizontal box layout on the left side of the pane.
+     * @return a HBox layout.
+     */
     public HBox getLeftControls() {
         return this.leftControls;
     }
 
+    /**
+     * An accessor to retrieve the horizontal box layout of the right side of the pane.
+     * @return a HBox layout.
+     */
     public HBox getRightControls() {
         return this.rightControls;
     }
 
+    /**
+     * A method to initialise layouts used within the pane.
+     */
     private void initialiseLayouts() {
         this.setPadding(new Insets(10));
         this.getStyleClass().addAll("background-secondary-border");
@@ -35,7 +50,10 @@ public class ActionPane extends BorderPane {
         this.rightControls.setSpacing(10);
     }
 
-    private void addComponentsToPane() {
+    /**
+     * A method to add the layouts to the pane.
+     */
+    private void addLayoutsToPane() {
         this.setLeft(this.leftControls);
         this.setRight(this.rightControls);
     }
