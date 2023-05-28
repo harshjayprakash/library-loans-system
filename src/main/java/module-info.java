e8f68ac03fe9e07ded952675b04b03b9ebec9e65
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-05-28 18:33:31 +0100

    Added export to every module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,6 @@
+/**
+ * Represents the libraries required for the winchester library module.
+ */
 module winchester.library {
     requires java.base;
     requires java.sql;
@@ -8,4 +11,17 @@ module winchester.library {
 
     opens winchester.library to javafx.controls;
     exports winchester.library;
+    exports winchester.library.data.access;
+    exports winchester.library.data.model.items;
+    exports winchester.library.data.model.users;
+    exports winchester.library.data.model.loans;
+    exports winchester.library.data.model.util;
+    exports winchester.library.meta;
+    exports winchester.library.service;
+    exports winchester.library.presentation.component.pane;
+    exports winchester.library.presentation.component.card;
+    exports winchester.library.presentation.view;
+    exports winchester.library.presentation.window;
+    exports winchester.library.presentation.alert;
+    exports winchester.library.presentation.style;
 }
